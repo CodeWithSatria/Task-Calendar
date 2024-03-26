c0fc75ec2f873c16e7f1654eef439180a2b8c9e4
CodeWithSatria <CodeWithSatria@gmail.com>
2024-03-26 15:58:47 +0800

    insert-1

---
+++ components/navbar.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const Navbar = () => {
+    return (
+        <nav className="flex h-[60px] border-b border-black font-bold">
+            <h1 className="flex items-center text-2xl text-black ml-[14px]">Satria's Calendar</h1>
+        </nav>
+    )
+}
+
+export default Navbar;
